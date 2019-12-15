048f9f5e70f851fe15b0e303c2d052205d88daa1
BadBonus <pikavit@tut.by>
2019-12-15 19:38:25 +0300

    added primitive loader

--- src/StreetsHelper.jsx
+++ src/StreetsHelper.jsx
@@ -5,7 +5,9 @@ class PlaceHelper extends React.Component{
 
     state = {
         inputAdress:'',
-        findedData:[]
+        findedData:[],
+        choosen:false,
+        onLoad:false
     };
 
     componentDidMount()
@@ -17,13 +19,15 @@ class PlaceHelper extends React.Component{
     takeData = (address) => {
         fetch(`http://photon.komoot.de/api/?q=${address}&limit=10`)
         .then((data)=>data.json())
-        .then(data=>this.setState({findedData:data.features}));
+        .then(data=>this.setState({findedData:data.features, choosen:false, onLoad:false}));
     }
+
+    checkOfUndef = (data) => data !== undefined ? data : '';
   
     timer = 0;
     render()
     {
-        
+       const checkOfUndef = this.checkOfUndef;
       return (
         <div className="Place-helper">
           <input 
@@ -33,25 +37,31 @@ class PlaceHelper extends React.Component{
             let value = e.target.value;
                 clearTimeout(this.timer);
                 this.timer = setTimeout(this.takeData, 800, value);
-            this.setState({inputAdress:value});
+                this.setState({inputAdress:value, onLoad:true});
             }}
            />
-        <ul>
         {
-        this.state.findedData.map((el)=>
+            !this.state.choosen &&
+            <ul>
             {
-                const {country, state, city, street, name, osm_id} = el.properties;
-                const fullData = `${country !== undefined && country} ${state !== undefined && state} ${city !== undefined && city} ${street !== undefined && street} ${name !== undefined && name}`;
-                return (
-                    <li onClick={()=>{this.setState({inputAdress:fullData})}} key={osm_id}>
-                        {
-                            fullData
-                        }
-                    </li>)
+            this.state.findedData.map((el)=>
+                {
+                    const {country, state, city, street, name, osm_id} = el.properties;
+                    const fullData = `${checkOfUndef(country)} ${checkOfUndef(state)} ${checkOfUndef(city)} ${checkOfUndef(street)} ${checkOfUndef(name)}`;
+                    return (
+                        <li onClick={()=>{this.setState({inputAdress:fullData, choosen:true})}} key={osm_id}>
+                            {
+                                fullData
+                            }
+                        </li>)
+                }
+            )
             }
-        )
+            </ul>
         }
-        </ul>
+        {
+            this.state.onLoad&& <span>Loading...</span>
+        }
         </div>
       );
     }
